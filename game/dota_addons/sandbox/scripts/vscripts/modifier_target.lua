920c93d635f8acea0772bfba7ee44042151bb25b
devilesk <devilesk@gmail.com>
2016-08-10 16:01:18 -0400

    change aggro highlight (+1 squashed commits)
    Squashed commits:
    
    [10b5964] change aggro highlight

--- game/dota_addons/sandbox/scripts/vscripts/modifier_target.lua
+++ game/dota_addons/sandbox/scripts/vscripts/modifier_target.lua
@@ -1,9 +1,13 @@
 modifier_target = class({})
 
+local AGGRO_COLOR = Vector(255, 0, 0)
+local NO_AGGRO_COLOR = Vector(0, 255, 0)
+
 function modifier_target:OnCreated(params)   
     if not IsServer() then
         return
     end
+    self._color = {}
     self._particles = {}
     self:StartIntervalThink (.01)
 end
@@ -12,9 +16,32 @@ function modifier_target:UpdateParticleForPlayer(i)
     local player = PlayerResource:GetPlayer(i)
     if player ~= nil then
         if GameRules.herodemo.overlays[i]["CreepAggroRangeButtonPressed"] == true then
+            if self._particles[i] ~= nil then
+                if self:GetParent():GetAggroTarget() ~= nil and self:GetParent():GetAggroTarget():GetPlayerOwnerID() == i then
+                   if self._color[i] ~= AGGRO_COLOR then
+                        ParticleManager:DestroyParticle(self._particles[i], true)
+                        ParticleManager:ReleaseParticleIndex(self._particles[i])
+                        self._particles[i] = nil
+                    end
+                    self._color[i] = AGGRO_COLOR
+                else
+                    if self._color[i] ~= NO_AGGRO_COLOR then
+                        ParticleManager:DestroyParticle(self._particles[i], true)
+                        ParticleManager:ReleaseParticleIndex(self._particles[i])
+                        self._particles[i] = nil
+                    end
+                    self._color[i] = NO_AGGRO_COLOR
+                end
+            end
+            
             if self._particles[i] == nil then
                 self._particles[i] = CreateParticleTarget(self:GetParent(), player)
+                if self._color[i] == nil then
+                    self._color[i] = NO_AGGRO_COLOR
+                end
             end
+
+            ParticleManager:SetParticleControl(self._particles[i], 1, self._color[i])
         elseif self._particles[i] ~= nil then
             ParticleManager:DestroyParticle(self._particles[i], true)
             ParticleManager:ReleaseParticleIndex(self._particles[i])
