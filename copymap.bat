51c8c40fee74edd2981b892dfcb9cfe43da52698
devilesk <devilesk@gmail.com>
2016-12-14 20:47:24 -0500

    add map copy script

---
+++ copymap.bat
@@ -0,0 +1,5 @@
+del /s /q "content\dota_addons\sandbox\maps"
+xcopy /s /y "C:\Program Files (x86)\Steam\steamapps\common\dota 2 beta\content\dota\maps" "content\dota_addons\sandbox\maps\"
+
+del /s /q "game\dota_addons\sandbox\maps"
+xcopy /s /y "C:\Program Files (x86)\Steam\steamapps\common\dota 2 beta\game\dota\maps\dota.vpk" "game\dota_addons\sandbox\maps\"
